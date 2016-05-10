8105ed0629e1bf1767be10abe29834747e22e0ac
Josh Washke <jwashke@icloud.com>
2016-05-10 11:47:00 -0600

    Use resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,13 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
 
+  namespace :store, path: ":store", as: :store do
+    resources :items, only: [:index, :show]
+    # get "items", to: "items#index", as: :items
+    # get "items/:id", to: "items#show", as: :item
+  end
+
   get ":store", to: "stores#show", as: :store
-  get ":store/items/:id", to: "store/items#show", as: :store_item
-  get ":store/items", to: "store/items#index", as: :store_items
+  # get ":store/items/:id", to: "store/items#show", as: :store_item
+  # get ":store/items", to: "store/items#index", as: :store_items
 end
