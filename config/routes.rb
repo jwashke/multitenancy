f0e00f031ebc13fccc4b93246234f756538c73eb
Josh Washke <jwashke@icloud.com>
2016-05-10 11:06:12 -0600

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root "stores#index"
+
+  resources :items,  only: [:index, :show]
+  resources :orders, only: [:index, :show]
+  resources :users,  only: [:index, :show]
+
+  get ":store", to: "stores#show", as: :store
+end
